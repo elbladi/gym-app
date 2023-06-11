bbc31f09e0a2cf2e9c30bb756d4b701e6e122cf7
Bladimir <oirbl@hotmail.com>
2023-06-11 11:20:32 -0600

    Config service

--- backend/favorites/src/app.module.ts
+++ backend/favorites/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import * as Joi from "joi";
+import { MongooseConfigService } from "./mongo.service";
+import { FavoritesModule } from "./favorites/favorites.module";
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid("development", "production", "test").default("development"),
+                MONGO_URI: Joi.string(),
+            }),
+        }),
+        MongooseModule.forRootAsync({
+            useClass: MongooseConfigService,
+            imports: [ConfigModule],
+        }),
+        FavoritesModule,
+    ],
 })
 export class AppModule {}
