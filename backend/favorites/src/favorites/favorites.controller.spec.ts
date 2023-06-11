0e169852b9f0c40f2a600f0a191af39cf53ae421
Bladimir <oirbl@hotmail.com>
2023-06-11 12:28:22 -0600

    🐛 fix setup file not found

--- backend/favorites/src/favorites/favorites.controller.spec.ts
+++ backend/favorites/src/favorites/favorites.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { FavoritesController } from './favorites.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { FavoritesController } from "./favorites.controller";
 
-describe('FavoritesController', () => {
-  let controller: FavoritesController;
+describe("FavoritesController", () => {
+    let controller: FavoritesController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [FavoritesController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [FavoritesController],
+        }).compile();
 
-    controller = module.get<FavoritesController>(FavoritesController);
-  });
+        controller = module.get<FavoritesController>(FavoritesController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(controller).toBeDefined();
+    });
 });
